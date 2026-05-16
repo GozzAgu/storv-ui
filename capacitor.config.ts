8df2750bf01f017d83d30581991656b6c5af47c3
GozzAgu <gozieagu1@gmail.com>
2026-05-16 21:49:00 +0100

    ..

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.storv.app',
-  appName: 'Storv',
+  appName: 'Storvv',
   webDir: 'dist',
   server: {
     iosScheme: 'capacitor',
